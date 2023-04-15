2c01df6ec0149415b21ae497671b430cfd1d4daa
Clarissa Verish <19670200+cverish@users.noreply.github.com>
2023-04-15 17:19:14 -0400

    Adding CORS

--- server/campsites_api/app.py
+++ server/campsites_api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from campsites_api.routers import campsites
 
@@ -8,6 +9,18 @@ def create_app() -> FastAPI:
         title="campsites",
         description="API for campsites in the US and Canada",
         version="1.0",
+    )
+
+    origins = [
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     @app.get("/health")
